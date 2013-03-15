ac72187b85bfd27a73e91db56812be482ed24a47
cmartin64x <cmartin@macadamian.com>
2013-03-15 14:35:30 -0400

    Temporary commit for library changes relate to moving QML file

--- odataTestHarness/translations/odataTestHarness.ts
+++ odataTestHarness/translations/odataTestHarness.ts
@@ -12,28 +12,13 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../assets/main.qml" line="7"/>
+        <location filename="../assets/main.qml" line="8"/>
         <source>Netflix</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../assets/main.qml" line="12"/>
+        <location filename="../assets/main.qml" line="13"/>
         <source>OData</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../assets/main.qml" line="17"/>
-        <source>Tab 3</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../assets/main.qml" line="23"/>
-        <source>Tab 3 title</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../assets/main.qml" line="38"/>
-        <source>Tab 3 content</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
